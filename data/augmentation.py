2c59b0d993beb62159ee0088fa9306e8a23afaa9
frshd <frshd@frshds-MacBook-Pro.local>
2024-06-05 22:28:53 -0400

    Remove invalid augmented chord sequences.

--- data/augmentation.py
+++ data/augmentation.py
@@ -43,9 +43,58 @@ def transpose_progression(progression, steps, use_flats=False):
     return transposed_chords
 
 
+def remove_unusual_transpositions(sequences):
+    roots_to_exclude = ['A#', 'B#', 'D#', 'E#']
+    res = []
+
+    for s in sequences:
+        should_remove = False
+        for w in s:
+            if any(root in w for root in roots_to_exclude):
+                should_remove = True
+        if not should_remove:
+            res.append(s)
+    return res
+
+
+def has_enharmonic_conflict(chord_progression):
+    natural_notes = {'C', 'D', 'E', 'F', 'G', 'A', 'B'}
+    sharps = {note + '#' for note in natural_notes}
+    flats = {note + 'b' for note in natural_notes}
+
+    roots = set()
+
+    for chord in chord_progression:
+        root = chord.split(':')[0]
+
+        if root in natural_notes:
+            if root + '#' in roots or root + 'b' in roots:
+                return True
+        elif root in sharps:
+            if root[0] in roots or root[0] + 'b' in roots:
+                return True
+        elif root in flats:
+            if root[0] in roots or root[0] + '#' in roots:
+                return True
+
+        roots.add(root)
+
+    return False
+
+
+def remove_invalid_enharmonic_sequences(sequences):
+    res = []
+    for s in sequences:
+        if not has_enharmonic_conflict(s):
+            res.append(s)
+    return res
+
+
 def get_chord_seq_in_different_keys(sequence):
     augmented_sequences = []
     for steps_to_transpose in range(12):
         augmented_sequences.append(transpose_progression(sequence, steps_to_transpose))
         augmented_sequences.append(transpose_progression(sequence, steps_to_transpose, use_flats=True))
-    return augmented_sequences
+    cleaned_augmented_sequences = remove_unusual_transpositions(augmented_sequences)
+    cleaned_augmented_sequences = remove_invalid_enharmonic_sequences(cleaned_augmented_sequences)
+    return cleaned_augmented_sequences
